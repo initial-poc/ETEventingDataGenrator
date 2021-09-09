76d6118cc7df8b25da0beabc04117c9c43efd776
initial-poc <chepuri.uk2021@gmail.com>
2021-09-09 15:12:56 +0530

    gcp project name changed

--- envDeployment.sh
+++ envDeployment.sh
@@ -2,6 +2,6 @@
 kubectl delete deployment et-traffice-generator-deployment
 git pull
 mvn clean package
-docker build . -t gcr.io/sab-order-service-sbx/et-traffice-generator
-docker push gcr.io/sab-order-service-sbx/et-traffice-generator:latest
+docker build . -t gcr.io/sab-order-service-sbx-7006/et-traffice-generator
+docker push gcr.io/sab-order-service-sbx-7006/et-traffice-generator:latest
 kubectl apply -f deployment.yaml
